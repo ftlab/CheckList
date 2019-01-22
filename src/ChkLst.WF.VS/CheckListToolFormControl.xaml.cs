1bfa9d1260499db3e52da12ecc8ebda25a1e5912
ulyanov yuriy <ulyanov.yuriy@live.com>
2019-01-22 22:59:31 +0300

    11

--- src/ChkLst.WF.VS/CheckListToolFormControl.xaml.cs
+++ src/ChkLst.WF.VS/CheckListToolFormControl.xaml.cs
@@ -1,7 +1,6 @@
 ﻿namespace ChkLst.WF.VS
 {
-    using System.Diagnostics.CodeAnalysis;
-    using System.Windows;
+    using ChkLst.WF.Views;
     using System.Windows.Controls;
 
     /// <summary>
@@ -15,20 +14,9 @@
         public CheckListToolFormControl()
         {
             this.InitializeComponent();
+
+            host.Child = new CheckListView();
         }
 
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
-        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "CheckListToolForm");
-        }
     }
 }
